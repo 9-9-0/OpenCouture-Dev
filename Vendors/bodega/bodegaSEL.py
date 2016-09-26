e4aa7ad65b423c217bb276ccaccb23df40ecbf0a
9-9-0 <max26814@gmail.com>
2016-09-25 22:52:57 -0500

    bodegaSEL.py(raw) WORKINGls!

--- Vendors/bodega/bodegaSEL.py
+++ Vendors/bodega/bodegaSEL.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
 from timeit import default_timer as timer
 import re
 import time
@@ -17,17 +18,30 @@ def transformSizeToClass(size):
     return output
 
 #Eventually let this function call find_element() instead of find_element_by_id()
-def attemptInput(idKey, value, maxAttempts):
+def attemptInput(by, key, value, maxAttempts):
+#def attemptInput(value, idKey, maxAttempts):
+    result = False
+    attempts = 0
+    while (attempts < maxAttempts):
+        try:
+            driver.find_element(by, key).send_keys(value)
+            result = True
+            break
+        except StaleElementReferenceException:
+            attempts = attempts + 1
+    return result
+'''
     result = False
     attempts = 0
     while (attempts < maxAttempts):
         try:
             driver.find_element_by_id(idKey).send_keys(value)
             result = True
-            break;
+            break
         except StaleElementReferenceException:
             attempts = attempts + 1
     return result
+'''
 
 URL_product_url = "https://shop.bdgastore.com/collections/footwear/products/y-3-qasa-boot"
 URL_home_url = "https://shop.bdgastore.com/collections/footwear"
@@ -123,6 +137,16 @@ cc_num = '4049379889248814'
 cc_name = 'Bob McFlymo'
 cc_expiry = '0318'
 cc_cvv = '233'
+bill_first_name = 'Bob'
+bill_last_name  = 'McFlymo'
+bill_company    = 'AyyLmao'
+bill_address1   = '2999 5th Ave'
+bill_address2   = 'Apt C'
+bill_city       = 'Seattle'
+bill_country    = 'United States'
+bill_state      = 'Washington'
+bill_zip        = '98101'
+bill_phone      = '2122229393'
 '''
 try:
     WebDriverWait(driver, 5).until(EC.presence_of_element_located(driver.find_element_by_id("number")))
@@ -135,38 +159,36 @@ time.sleep(1)
 #For now, using this quick hack. It'd be nice to be able to find the custom keys that come after the card-fields-[key] that get generated
 #NOTE: Issue with entering CC num, class of input element changes based on first 4 digits and thus, the element becomes stale
 
-driver.find_element_by_xpath("//iframe[starts-with(@name, 'card-fields-iframe-
-'''
-for iframe in driver.find_elements_by_class_name("card-fields-iframe"):
-    if 'name' in iframe.get_attribute("id"):
-        print 'Name field found'
-        driver.switch_to_frame(iframe)
-        driver.find_element_by
-        continue
-    elif 'expiry' in iframe.get_attribute("id"):
-        print 'Expiry field found'
-        continue
-    elif 'verification' in iframe.get_attribute("id"):
-        print 'Verification value found'
-        continue
-'''
-'''
-    #print iframe.get_attribute("id")
-    if 'number' in iframe.get_attribute("id"):
-        print 'Number field found'
-        driver.switch_to_frame(iframe)
+#Input CC num
+num_iframe = driver.find_element_by_xpath("//iframe[starts-with(@name, 'card-fields-number')]")
+driver.switch_to_frame(num_iframe)
+for digit in cc_num:
+    attemptInput(By.ID, "number", digit, 5)
+    #attemptInput(digit, "number", 5)
+driver.switch_to_default_content()
+print 'Did we get here?'
 
-        for digit in cc_num:
-            result = attemptInput("number", digit, 5)
-            print result
-            driver.switch_to_default_content
-        continue
-'''
-'''
+#Input CC name
+name_iframe = driver.find_element_by_xpath("//iframe[starts-with(@name, 'card-fields-name')]")
+driver.switch_to_frame(name_iframe)
 driver.find_element_by_id("name").send_keys(cc_name)
-driver.find_element_by_id("expiry").send_keys(cc_expiry)
-driver.find_element_by_id("verification_value").send_keys(cc_cvv)
+driver.switch_to_default_content()
 
+#Input CC expiry
+expiry_iframe = driver.find_element_by_xpath("//iframe[starts-with(@name, 'card-fields-expiry')]")
+driver.switch_to_frame(expiry_iframe)
+for digit in cc_expiry:
+    attemptInput(By.ID, "expiry", digit, 5)
+#driver.find_element_by_id("expiry").send_keys(cc_expiry)
+driver.switch_to_default_content()
+
+#Input CC cvv
+cvv_iframe = driver.find_element_by_xpath("//iframe[starts-with(@name, 'card-fields-verification')]")
+driver.switch_to_frame(cvv_iframe)
+driver.find_element_by_id("verification_value").send_keys(cc_cvv)
+driver.switch_to_default_content()
+
+#Input Billing Address
 driver.find_element_by_id("checkout_billing_address_first_name").send_keys(bill_first_name)
 driver.find_element_by_id("checkout_billing_address_last_name").send_keys(bill_last_name)
 driver.find_element_by_id("checkout_billing_address_company").send_keys(bill_company)
@@ -183,8 +205,31 @@ driver.find_element_by_id("checkout_billing_address_zip").send_keys(bill_zip)
 driver.find_element_by_id("checkout_billing_address_phone").send_keys(bill_phone)
 
 driver.find_element_by_name("button").click()
-'''
 
+'''
+for iframe in driver.find_elements_by_class_name("card-fields-iframe"):
+    if 'name' in iframe.get_attribute("id"):
+        print 'Name field found'
+        driver.switch_to_frame(iframe)
+        driver.find_element_by
+        continue
+    elif 'expiry' in iframe.get_attribute("id"):
+        print 'Expiry field found'
+        continue
+    elif 'verification' in iframe.get_attribute("id"):
+        print 'Verification value found'
+        continue
+    #print iframe.get_attribute("id")
+    if 'number' in iframe.get_attribute("id"):
+        print 'Number field found'
+        driver.switch_to_frame(iframe)
+
+        for digit in cc_num:
+            result = attemptInput("number", digit, 5)
+            print result
+            driver.switch_to_default_content
+        continue
+'''
 
 end = timer()
 print "Runtime: " + str(end - start) + "s"
